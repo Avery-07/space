53c45454e99308a67a8e3be1c9eaf02d004b1531
Avery <mayeul.degans@gmail.com>
2026-02-15 21:31:54 +0100

    Project Creation

---
+++ src/main/java/util/Main.java
@@ -0,0 +1,32 @@
+package util;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import menus.MainMenu;
+
+public class Main extends Application {
+
+    private static final int WINDOW_WIDTH  = 800;
+    private static final int WINDOW_HEIGHT = 600;
+    private static final String WINDOW_TITLE = "Mon Jeu";
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Construction du menu principal
+        MainMenu mainMenu = new MainMenu(primaryStage);
+
+        // Création de la scène avec la vue du menu
+        Scene scene = new Scene(mainMenu.getView(), WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        // Configuration de la fenêtre
+        primaryStage.setTitle(WINDOW_TITLE);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
